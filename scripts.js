a0b33c024cc3988bc9ec684ae5acb279fb0a9b95
Hunter Bossetto <hbstudentswic@gmail.com>
2021-04-19 11:50:24 -0500

    Import from Codepen.io

--- scripts.js
+++ scripts.js
@@ -1,1 +1,25 @@
+const p = document.querySelector('p');
 
+const root = document.querySelector(':root');
+
+const input = document.getElementById('input');
+
+const range = document.getElementById('range-slider');
+
+const color = document.getElementById('color-changer');
+
+input.addEventListener('keyup', () => {
+  const inputValue = document.getElementById('input').value;
+
+  /* console.log(inputValue); JUST FOR TESTING */
+
+  p.innerText = inputValue;
+});
+
+range.addEventListener('input', event => {
+  root.style.setProperty('--font-size', `${event.target.value}px`);
+});
+
+color.addEventListener('input', event => {
+  root.style.setProperty('--color', event.target.value);
+});
